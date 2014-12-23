0ec47fe609881d1d80e9b8b978fee8ed4c08baef
Boris Yankov <borisyankov@gmail.com>
2014-12-23 08:12:26 +0200

    Correctly determine IP and store

--- server/api/contact.js
+++ server/api/contact.js
@@ -3,47 +3,37 @@ var Firebase = require('firebase');
 var parse = require('co-body');
 var mandrill = require('node-mandrill')('EDUnJcByBGdZM9_H0lk2og');
 
-module.exports = function(app, config, errors) {
+module.exports = function *(next) {
 
+    var firebase = new Firebase("https://leanmetrix.firebaseio.com/" + this.request.host + '/messages/');
 
-    app.use(router.post('/contact', function *(next) {
+    var body = yield parse(this);
 
-        var firebase = new Firebase("https://leanmetrix.firebaseio.com/" + this.request.host + '/messages/');
+    firebase.push().set({
+        timestamp: Firebase.ServerValue.TIMESTAMP,
+        name: body.name,
+        email: body.email,
+        message: body.message,
+        url: this.url,
+        ip: this.ip
+    });
 
-        var body = yield parse(this);
+    mandrill('/messages/send', {
+        message: {
+            to: [{
+                email: 'borisyankov@gmail.com',
+                name: 'Boris Yankov'
+            }],
+            from_email: body.email,
+            subject: 'New message form ' + body.name,
+            text: body.message
+        }
+    }, function(error, response) {
+        //uh oh, there was an error
+        if (error) console.log(JSON.stringify(error));
 
-        var ip = this.request.headers['x-forwarded-for'] ||
-            this.request.connection.remoteAddress ||
-            this.request.socket.remoteAddress ||
-            this.request.connection.socket.remoteAddress;
-
-        firebase.push().set({
-            timestamp: Firebase.ServerValue.TIMESTAMP,
-            name: body.name,
-            email: body.email,
-            message: body.message,
-            url: this.request.url,
-            ip: ip
-        });
-
-        mandrill('/messages/send', {
-            message: {
-                to: [{
-                    email: 'borisyankov@gmail.com',
-                    name: 'Boris Yankov'
-                }],
-                from_email: body.email,
-                subject: 'New message form ' + body.name,
-                text: body.message
-            }
-        }, function(error, response) {
-            //uh oh, there was an error
-            if (error) console.log(JSON.stringify(error));
-
-                //everything's good, lets see what mandrill said
-            else console.log(response);
-        });
-
-    }));
+            //everything's good, lets see what mandrill said
+        else console.log(response);
+    });
 
 };
